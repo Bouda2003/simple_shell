c8bf41e7df60243d5ec6e9c21906ba1f6de2a671
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2024-02-27 19:35:04 -0800

    done

--- get_command.c
+++ get_command.c
@@ -10,13 +10,20 @@ char *get_input()
 
 	if (buffer == NULL)
 	{
-		perror("unsuccessful process");
+		perror("unsuccessful allocation process");
 		return (NULL);
 	}
 
 	printf("$ ");
 	fgets(buffer, MAX_COMMAND_LENGTH, stdin);
 	char **args = malloc(MAX_ARGS * sizeof(char *));
+
+	if (args == NULL)
+	{
+		perror("unsuccessful allocation process for args");
+		free(buffer);
+		return (NULL);
+	}
 	int i = 0;
 	char *token = strtok(buffer, " ");
 
@@ -25,5 +32,9 @@ char *get_input()
 		args[i++] = token;
 		token = strtok(NULL, " ");
 	}
+	if (token != NULL)
+		fprintf(stderr, "too many arguments\n");
+	free(args);
+	free(buffer);
 	return (args);
 }
